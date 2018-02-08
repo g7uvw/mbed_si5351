7dede43bab35e6003bff358120dfe15125231310
David <dave@webshed.org>
2018-02-08 20:59:10 +0000

    Working, same results as arduino code gives

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,58 @@
 #include "mbed.h"
 #include "si5351.h"
 
-
 Serial pc(SERIAL_TX, SERIAL_RX);
 Si5351 si5351;
+
+void loop(void);
 
 int main() {
 
 bool i2c_found;
-uint64_t freq_a = 1e7;
-uint64_t freq_b = 1e5;
+uint64_t freq_a = 1000000000ULL;
+uint64_t freq_b = 17500000000ULL;
 pc.printf("Hello World!\n");
 
 // Start serial and initialize the Si5351
     i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
-    if(!i2c_found) {
-        pc.printf("\nDevice not found on I2C bus!\n");
+    if(!i2c_found)
+    {
+        pc.printf("\nSI5351 Device found on I2C bus!\n");
     }
-//loop_start:
-  // Set CLK0 to output 14 MHz
-  wait(1);
-  si5351.set_freq(1400000000ULL, SI5351_CLK0);
-  //si5351.set_freq(freq_a, SI5351_CLK0);
-  si5351.update_status();
+    else
+    {
+      pc.printf("\nDevice not found on I2C bus!\n");
+      pc.printf("Entering do-nothing loop\n");
+      for(;;);
+    }
 
-  //wait(1);
-  //si5351.set_freq(freq_b, SI5351_CLK0);
+  // Set CLK0 to output 10 MHz
+  si5351.set_freq(freq_a, SI5351_CLK0);
 
-
-  //wait(5);
-  //si5351.set_freq(freq_b, SI5351_CLK0);
   // Set CLK1 to output 175 MHz
-  //si5351.set_ms_source(SI5351_CLK1, SI5351_PLLB);
-  //si5351.set_freq_manual(freq_b, 70000000000ULL, SI5351_CLK1);
+  si5351.set_ms_source(SI5351_CLK1, SI5351_PLLB);
+  si5351.set_freq_manual(freq_b, 70000000000ULL, SI5351_CLK1);
 
-    // Query a status update and wait a bit to let the Si5351 populate the
-    // status flags correctly.
-  //si5351.update_status();
-  //wait(5);
-  //goto loop_start;
+loop();
+
+}
+
+void loop(void)
+{
+  loop_start:
+// Read the Status Register and print it every 10 seconds
+ si5351.update_status();
+ pc.printf("SYS_INIT: %d \n", si5351.dev_status.SYS_INIT);
+ //pc.printf(si5351.dev_status.SYS_INIT);
+ pc.printf("  LOL_A: %d \n", si5351.dev_status.LOL_A);
+ //pc.printf(si5351.dev_status.LOL_A);
+ pc.printf("  LOL_B: %d \n", si5351.dev_status.LOL_B);
+ //pc.printf(si5351.dev_status.LOL_B);
+ pc.printf("  LOS: %d \n", si5351.dev_status.LOS);
+ //pc.printf(si5351.dev_status.LOS);
+ pc.printf("  REVID: %d \n", si5351.dev_status.REVID);
+ //pc.printf(si5351.dev_status.REVID);
+
+ wait(5);
+ goto loop_start;
 }
--- src/si5351.cpp
+++ src/si5351.cpp
@@ -65,16 +65,6 @@ Si5351::Si5351(uint8_t i2c_addr):
  */
 bool Si5351::init(uint8_t xtal_load_c, uint32_t xo_freq, int32_t corr)
 {
-    // Start I2C comms
-    //Wire.begin();
-
-    // Check for a device on the bus, bail out if it is not there
-    //i2c.beginTransmission(i2c_bus_addr);
-    //uint8_t reg_val;
-  //reg_val = Wire.endTransmission();
-
-    //if(reg_val == 0)
-    //{
         // Wait for SYS_INIT flag to be clear, indicating that device is ready
         uint8_t status_reg = 0;
         do
@@ -101,11 +91,7 @@ bool Si5351::init(uint8_t xtal_load_c, uint32_t xo_freq, int32_t corr)
         reset();
 
         return status_reg;
-    //}
-    //else
-    //{
-    //    return false;
-    //}
+
 }
 
 /*
